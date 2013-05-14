ecf712071581245d9f4f55eb8b549f0d3f686a3d
george.fandli@illum.io <gfandli@dev3.illum.io>
2013-05-14 10:42:54 -0700

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LunchLearn::Application.config.secret_token = '373bca6ff467fd5dbb6348c754fbe0d1d9b56b8bb576b23bee5893070590d46772ce26e5fff80327374363d365d4d88aa2b2cbfc494e90dd6e24465fd510e0e4'
